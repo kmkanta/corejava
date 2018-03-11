bf2cc204190b82b963855396553f219c235ba857
kmkanta <kandulamanikanta7@gmail.com>
2018-03-11 07:53:22 +0530

    To check Even or Odd

---
+++ src/com/corejava/ToCheckEvenOrOdd.java
@@ -0,0 +1,22 @@
+package com.corejava;
+
+import java.util.Scanner;
+
+public class ToCheckEvenOrOdd {
+	public void toCheckEvenOrOdd() {
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter any Number");
+		int number = sc.nextInt();
+		
+		if(number%2==0) {
+			System.out.println(number+" is Even");
+		}else {
+			System.out.println(number+" is Odd");
+		}
+	}
+public static void main(String[] args) {
+	ToCheckEvenOrOdd obj = new ToCheckEvenOrOdd();
+	obj.toCheckEvenOrOdd();
+}
+}
